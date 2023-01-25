73b62a72f4fac9ddb324fc082c0be687225317da
Ernesto Cardenas <71235388+Ernestocardenas@users.noreply.github.com>
2023-01-24 22:45:45 -0500

    all files uploaded

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
-let hours = ['6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM'];
+// Sourced from MDN Web Docs - Math.random
+var hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
 
 function getRandomIntInclusive(min, max) {
-  return Math.floor(Math.random) * (max - min + 1) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
 
-function cities(name, minCust, maxCust, averageCookieSale, openTime, closeTime){
+function Cities(name, minCust, maxCust, averageCookieSale, openTime, closeTime) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
@@ -17,29 +18,27 @@ function cities(name, minCust, maxCust, averageCookieSale, openTime, closeTime){
   this.dailyTotal = 0;
 }
 
-// function for cookie per hour
-
-cities.prototype.calcCookiePerHour = function () {
-  for (let i = 0; i < 14; i++) {
+Cities.prototype.calcCookiesPerHour = function () {
+  for (var i = 0; i < 14; i++) {
     this.hourlyTotal[i] = Math.ceil(getRandomIntInclusive(this.minCust, this.maxCust) * this.averageCookieSale);
     this.dailyTotal += this.hourlyTotal[i];
     console.log(this.hourlyTotal[i]);
   }
 };
 
-cities.prototype.render = function () {
+Cities.prototype.render = function () {
   this.calcCookiesPerHour();
-  let tableEl = document.getElementById('dataTable');
+  var tableEl = document.getElementById('dataTable');
 
-  let trElement = document.createElement('tr');
+  var trElement = document.createElement('tr');
   tableEl.appendChild(trElement);
 
-  let thElement = document.createElement('th');
+  var thElement = document.createElement('th');
   thElement.textContent = this.name;
   trElement.appendChild(thElement);
 
-  for (let j = 0; j < this.hourlyTotal.length; j++) {
-    let tdElement = document.createElement('td');
+  for (var j = 0; j < this.hourlyTotal.length; j++) {
+    var tdElement = document.createElement('td');
     tdElement.textContent = this.hourlyTotal[j];
     trElement.appendChild(tdElement);
   }
@@ -50,48 +49,47 @@ cities.prototype.render = function () {
   tableEl.appendChild(trElement);
 };
 
-let seattle = new cities('Seattle', 23, 65, 6.3, 6, 20);
-let tokyo = new cities('Tokyo', 3, 24, 1.2, 6, 20);
-let dubai = new cities('Dubai', 11, 38, 3.7, 6, 20);
-let paris = new cities('Paris', 20, 38, 2.3, 6, 20);
-let lima = new cities('Lima', 2, 16, 4.6, 6, 20);
+var seattle = new Cities('Seattle', 23, 65, 6.3, 6, 20);
+var tokyo = new Cities('Tokyo', 3, 24, 1.2, 6, 20);
+var dubai = new Cities('Dubai', 11, 38, 3.7, 6, 20);
+var paris = new Cities('Paris', 20, 38, 2.3, 6, 20);
+var lima = new Cities('Lima', 2, 16, 4.6, 6, 20);
 
-let city = [seattle, tokyo, dubai, paris, lima];
+var city = [seattle, tokyo, dubai, paris, lima];
 
-for (let i = 0; i < city.length; i++){
-  city[i].calcCookiePerHour();
+for (var k = 0; k < city.length; k++) {
+  city[k].calcCookiesPerHour();
 }
 
-let tableBody = document.getElementById('dataTable');
+var tableBody = document.getElementById('dataTable');
 
 function renderheader() {
-  let headerrow = document.createElement('tr');
-  let headerstore = document.createElement('th');
+  var headerrow = document.createElement('tr');
+  var headerstore = document.createElement('th');
   headerstore.textContent = 'Locations';
   headerrow.appendChild(headerstore);
   tableBody.appendChild(headerrow);
-
-  for (let i = 0; i < hours.length; i++) {
-    let headerhour = document.createElement('th');
+  for (var i = 0; i < hours.length; i++) {
+    var headerhour = document.createElement('th');
     headerhour.textContent = hours[i];
     headerstore.appendChild(headerhour);
     headerrow.appendChild(headerhour);
   }
-  let headertotal = document.createElement('th');
+  var headertotal = document.createElement('th');
   headertotal.textContent = 'Store Totals';
   headerrow.appendChild(headertotal);
 }
 
 renderheader();
-for (let i = 0; i < city.length; i++) {
+for (var i = 0; i < city.length; i++) {
   city[i].render();
 }
 renderFooter();
 
 function renderFooter() {
-  let headerrow = document.createElement('tr');
+  var headerrow = document.createElement('tr');
   tableBody.appendChild(headerrow);
-  let tdElement = document.createElement('td');
+  var tdElement = document.createElement('td');
 
   tdElement.textContent = 'Daily Total';
   headerrow.appendChild(tdElement);
